98a08e7b22364f30edf4ea178b32990838ca6367
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-29 18:38:47 -0400

    table display populate

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,6 +6,8 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,4 +29,20 @@ public class UserController {
 
         return "/user/create";
     }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user")UserDTO user, Model model){
+
+
+        userService.save(user);
+
+
+        return "redirect:/user/create";
+    }
+
+
+
+
+
+
 }
