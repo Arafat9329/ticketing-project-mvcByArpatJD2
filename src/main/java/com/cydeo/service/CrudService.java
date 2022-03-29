84d9a3a0c534e22eaa652ada224d6987e20d42cd
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-28 21:55:00 -0400

    part-1 impl

--- src/main/java/com/cydeo/service/CrudService.java
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -1,6 +1,5 @@
 package com.cydeo.service;
 
-
 import java.util.List;
 
 public interface CrudService<T,ID> {
--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,11 +1,9 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.RoleDTO;
-import com.cydeo.dto.UserDTO;
-
-import java.util.List;
 
 //save,findBy,delete,findAll
 public interface RoleService extends CrudService<RoleDTO,Long>{
 
+    RoleDTO myLogic(RoleDTO roleDTO,String str);
 }
