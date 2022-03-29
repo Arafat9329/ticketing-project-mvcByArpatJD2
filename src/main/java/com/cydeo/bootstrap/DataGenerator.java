d5d9228e2c74161882bfddc656143c583ea52f03
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-28 23:55:30 -0400

    sporting bootstrap injection

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,9 +1,19 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
 import org.springframework.boot.CommandLineRunner;// run it when/before the application run
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class DataGenerator implements CommandLineRunner {
+
+    RoleService roleService; //injection
+
+    public DataGenerator(RoleService roleService) {
+        this.roleService = roleService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
