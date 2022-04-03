8b62d333d9fc8a0cccab4401125bf6a722fb74df
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-04-03 15:51:28 -0400

    Task page--> select project

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,5 +1,9 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,11 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/task")
+@RequestMapping("/task")//http://localhost:8080/task
 public class TaskController {
 
-    @GetMapping("/create")
+    TaskService taskService;
+    UserService userService;
+    ProjectService projectService;
+
+    public TaskController(TaskService taskService, UserService userService, ProjectService projectService) {
+        this.taskService = taskService;
+        this.userService = userService;
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/create")//http://localhost:8080/task/create
     public String createTask(Model model){
+
+        model.addAttribute("task",new TaskDTO());
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("users",userService.findAll());
+
         return "task/create";
     }
 }
