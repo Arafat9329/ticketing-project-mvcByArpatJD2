2eeed2ac00fb5fde9a8c719b5f16568bcf19a724
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-04-03 12:54:25 -0400

    completed project

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -46,4 +46,13 @@ public class ProjectController {
 
         return "redirect:/project/create";
     }
+
+
+    @GetMapping("/complete/{projectcode}")
+    public String completeProject(@PathVariable("projectcode") String projectcode){
+
+        projectService.complete(projectService.findById(projectcode));
+
+        return "redirect:/project/create";
+    }
 }
