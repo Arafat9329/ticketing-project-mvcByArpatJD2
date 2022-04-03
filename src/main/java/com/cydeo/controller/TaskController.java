9adf3aafc1cbda4fa3b322e7f00c90cbcf051da8
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-04-03 16:28:58 -0400

    Task page--> ProjectDtoConverter

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -33,5 +36,15 @@ public class TaskController {
         model.addAttribute("tasks",taskService.findAll());
 
         return "task/create";
+    }
+
+    @PostMapping("/create")
+//    public String insertTask(@ModelAttribute("task") TaskDTO task){
+    public String insertTask(TaskDTO task){
+
+        taskService.save(task);
+
+        return "redirect:/task/create";
+
     }
 }
